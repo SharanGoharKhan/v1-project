cdf6801a991992c3a4bc8edcf41759b729295f10
Sharan Khan <sharan.gohar@gmail.com>
2019-02-23 17:05:03 +0500

    singleitem update

--- v1-app/src/components/Admin/ItemList/SingleItem/SingleItem.js
+++ v1-app/src/components/Admin/ItemList/SingleItem/SingleItem.js
@@ -1,13 +1,80 @@
 import React from 'react'
 
-const singleItem = props => (
-    <div className="card">
-        <div className="card-body">
-            <h5 className="card-title">{props.title}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{props.price}</h6>
-            <p className="card-text">{props.desc}</p>
-            <a className="card-link">Updated</a>
-        </div>
-    </div>
-)
-export default singleItem;
+
+class SingleItem extends React.Component {
+
+    state = {
+        title: this.props.title,
+        desc: this.props.desc,
+        price: this.props.price
+
+    }
+    constructor(props) {
+        super(props)
+        this.onFormSubmit = this.onFormSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+
+    }
+    handleChange(event) {
+        const field_name = event.target.name;
+        const field_value = event.target.value;
+        const obj = {
+            [field_name]: field_value
+        }
+        this.setState(
+            obj
+        )
+    }
+    onFormSubmit(event) {
+        event.preventDefault();
+        const title = this.state.title
+        const desc = this.state.desc
+        const price = +this.state.price
+
+        if (
+            title.trim().length === 0 ||
+            price <= 0 ||
+            desc.trim().length === 0
+        ) {
+            return
+        }
+        const item = { title, desc, price };
+        console.log(item)
+    }
+
+    render() {
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <form onSubmit={this.onFormSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="itemTitle">Item Title</label>
+                            <input className="form-control" 
+                            value={this.state.title}
+                            name="title" 
+                            onChange={this.handleChange} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="itemDescription">Item Description</label>
+                            <input className="form-control" 
+                            value={this.state.desc}
+                            name="desc"
+                            onChange={this.handleChange} />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="itemDescription">Item Price</label>
+                            <input type="number" step="0.0001" 
+                            className="form-control"
+                            name="price" 
+                            value={this.state.price} 
+                            onChange={this.handleChange}/>
+                        </div>
+                        <button className="btn btn-primary">Update</button>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
+}
+export default SingleItem;
