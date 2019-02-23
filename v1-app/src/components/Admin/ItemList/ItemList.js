0e4dc0fb62d5c31a91e766cebe758933e0459b00
Sharan Khan <sharan.gohar@gmail.com>
2019-02-23 15:52:56 +0500

    updates

--- v1-app/src/components/Admin/ItemList/ItemList.js
+++ v1-app/src/components/Admin/ItemList/ItemList.js
@@ -4,11 +4,13 @@ import SingleItem from './SingleItem/SingleItem'
 const itemList = props => {
     const items = props.items.map(item => {
         return (
-            <SingleItem
-            title={item.title}
-            desc={item.desc}
-            price={item.price}
-            />
+            <div className="marginTopBot">
+                <SingleItem
+                title={item.title}
+                desc={item.desc}
+                price={item.price}
+                />
+            </div>
         )
     })
     return <div>{items}</div>
--- v1-app/src/components/Admin/ItemList/SingleItem/SingleItem.js
+++ v1-app/src/components/Admin/ItemList/SingleItem/SingleItem.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const singleItem = props => (
-    <div>
-        <h1>{props.title}</h1>
-        <h2>{props.desc}</h2>
-        <h3>{props.price}</h3>
+    <div className="card">
+        <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{props.price}</h6>
+            <p className="card-text">{props.desc}</p>
+            <a className="card-link">Updated</a>
+        </div>
     </div>
 )
 export default singleItem;
